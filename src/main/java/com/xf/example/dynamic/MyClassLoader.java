8f6451c0ef4e52158fd271660984a7664a894d8a
Administrator <870581857@qq.com>
2018-11-22 00:20:24 +0800

    dynamic

---
+++ src/main/java/com/xf/example/dynamic/MyClassLoader.java
@@ -0,0 +1,12 @@
+package com.xf.example.dynamic;
+
+/**
+ * 自定义一个类加载器，用于将字节码转换为class对象
+ */
+public class MyClassLoader extends ClassLoader {
+
+    public Class<?> defineMyClass(byte[] b, int off, int len) {
+        return super.defineClass("com.xf.example.dynamic.Programmer", b, off, len);
+    }
+
+}
---
+++ src/main/java/com/xf/example/dynamic/Programmer.java
@@ -0,0 +1,11 @@
+package com.xf.example.dynamic;
+
+/**
+ * 程序猿类
+ */
+public class Programmer {
+
+    public void code() {
+        System.out.println("I'm a Programmer,Just Coding.....");
+    }
+}
