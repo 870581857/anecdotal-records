b87409bb0f03f69b33336a2df8d7b3aa17ebb79c
Administrator <870581857@qq.com>
2019-01-08 00:02:06 +0800

    thread

---
+++ src/main/java/com/xf/example/thread/executor/CachedThreadPool.java
@@ -0,0 +1,15 @@
+package com.xf.example.thread.executor;
+
+import com.xf.example.thread.LiftOff;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i=0;i<5;i++)
+            exec.execute(new LiftOff());
+        exec.shutdown();
+    }
+}
---
+++ src/main/java/com/xf/example/thread/executor/FixedThreadPool.java
@@ -0,0 +1,16 @@
+package com.xf.example.thread.executor;
+
+import com.xf.example.thread.LiftOff;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FixedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(5);
+        for(int i=0;i<5;i++){
+            exec.execute(new LiftOff());
+        }
+        exec.shutdown();
+    }
+}
